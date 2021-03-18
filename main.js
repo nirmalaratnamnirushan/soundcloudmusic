7ef731588a7ae63d1b232bf2cb518973a6d5a6bb
nirmalaratnamnirushan <ni3niru3@gmail.com>
2021-03-19 01:09:15 +0530

    úpdated

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
         });
 
     });
-    SC.stream('/tracks/971028916', function (sound) {
+    SC.stream('/tracks/979934314', function (sound) {
 
         $('#sta1').click(function (e) {
             e.preventDefault();
@@ -30,41 +30,41 @@ $(document).ready(function () {
         });
 
     });
-    SC.stream('/tracks/556842441', function (sound) {
+    SC.stream('/tracks/979540036', function (sound) {
 
-        $('#start2').click(function (e) {
+        $('#sta2').click(function (e) {
             e.preventDefault();
             sound.start();
         });
 
-        $('#stop2').click(function (e) {
+        $('#sto2').click(function (e) {
             e.preventDefault();
             sound.stop();
         });
 
     });
-    SC.stream('/tracks/345819962', function (sound) {
+    SC.stream('/tracks/498415362', function (sound) {
 
-        $('#start3').click(function (e) {
+        $('#sta3').click(function (e) {
             e.preventDefault();
             sound.start();
         });
 
-        $('#stop3').click(function (e) {
+        $('#sto3').click(function (e) {
             e.preventDefault();
             sound.stop();
         });
 
     });
   
-    SC.stream('/tracks/649577930', function (sound) {
+    SC.stream('/tracks/551558922', function (sound) {
 
-        $('#start5').click(function (e) {
+        $('#sta4').click(function (e) {
             e.preventDefault();
             sound.start();
         });
 
-        $('#stop5').click(function (e) {
+        $('#sto4').click(function (e) {
             e.preventDefault();
             sound.stop();
         });
@@ -72,4 +72,7 @@ $(document).ready(function () {
     });
     });
 
-    //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/971028916&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-490378388" title="ECSTAPSY" target="_blank" style="color: #cccccc; text-decoration: none;">ECSTAPSY</a> · <a href="https://soundcloud.com/user-490378388/ecstasy-master-the-blaster" title="Master - Master The Blaster | Ecstapsy Remix" target="_blank" style="color: #cccccc; text-decoration: none;">Master - Master The Blaster | Ecstapsy Remix</a></div>
+    //<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/979934314&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/ashxelo-musical" title="Ashvin Bala" target="_blank" style="color: #cccccc; text-decoration: none;">Ashvin Bala</a> · <a href="https://soundcloud.com/ashxelo-musical/pona-pogattum-mashup-cover" title="Pona Pogattum MashUp Cover" target="_blank" style="color: #cccccc; text-decoration: none;">Pona Pogattum MashUp Cover</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/979540036&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-403218881" title="Ajai manimala" target="_blank" style="color: #cccccc; text-decoration: none;">Ajai manimala</a> · <a href="https://soundcloud.com/user-403218881/maara-yaar-azhaippadhu" title="Yaar Azhaippadhu - Maara" target="_blank" style="color: #cccccc; text-decoration: none;">Yaar Azhaippadhu - Maara</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/551558922&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/filmy-songs" title="Filmy Songs" target="_blank" style="color: #cccccc; text-decoration: none;">Filmy Songs</a> · <a href="https://soundcloud.com/filmy-songs/dheera-dheera-anaya-bhatt-kgf-telugu-filmysongs" title="Dheera Dheera Anaya Bhatt KGF Telugu Filmysongs" target="_blank" style="color: #cccccc; text-decoration: none;">Dheera Dheera Anaya Bhatt KGF Telugu Filmysongs</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/498415362&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/mathianbu" title="Mathi Anbu" target="_blank" style="color: #cccccc; text-decoration: none;">Mathi Anbu</a> · <a href="https://soundcloud.com/mathianbu/kaathalae-kaathalae" title="Kaathalae Kaathalae" target="_blank" style="color: #cccccc; text-decoration: none;">Kaathalae Kaathalae</a></div>
